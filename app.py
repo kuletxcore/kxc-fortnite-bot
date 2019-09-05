fd30504b172aeeb80694eaf4ae752e6d0b160222
unknown <kuletxcore@gmail.com>
2019-09-05 16:18:38 +0800

    set time
    This bot will now only run per 24 hours

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
 OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
-TWEET_LENGTH = 140
+TWEET_LENGTH = 280
 TWEET_URL_LENGTH = 21
 
-RUN_EVERY_N_SECONDS = 60*5 # e.g. 60*5 = tweets every five minutes
+RUN_EVERY_N_SECONDS = 86400 # e.g. 60*5 = tweets every five minutes
 
 USERS_TO_IGNORE = []
 DO_NOT_FAVORITE_USERS_AGAIN = True
@@ -29,7 +29,7 @@ def random_favoriting(keywords, handle, favorite_probability=0.2):
         then you can expect to favorite a tweet
         once every N/favorite_probability seconds
     """
-    for keyword in keywords:
+	for keyword in keywords:
         xs = handle.search(q=keyword)
         ts = [x for x in xs['statuses']]
         if DO_NOT_FAVORITE_USERS_AGAIN:
