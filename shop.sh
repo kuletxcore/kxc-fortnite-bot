b4ea35316deac413bd8a5c2ac481f8b21a461b05
unknown <kuletxcore@gmail.com>
2019-09-06 13:11:45 +0800

    sh files do not work for some reason...

--- shop.sh
+++
@@ -1,6 +0,0 @@
-cd ./output
-rm news.png
-sleep 15
-curl "http://api2.nitestats.com/v1/news/image?background=https://github.com/kuletxcore/kxc-fortnite-bot/raw/master/media/news.jpg" --output news.png
-cd ..
-python news.py
