73e9b751335b82742ca67440167062251e7a935b
kuletxcore <kuletxcore@gmail.com>
2019-09-08 11:33:52 +0800

    I did an oopsie
    Accidentally swapped the shop and news images

--- twitterbot.sh
+++ twitterbot.sh
@@ -3,12 +3,12 @@ cd output
 rm news.png
 rm shop.png
 sleep 15
-curl "http://api2.nitestats.com/v1/news/image?background=https://github.com/kuletxcore/kxc-fortnite-bot/raw/master/media/news.jpg" --output news.png
+curl "http://api2.nitestats.com/v1/itemshop?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
 cd ..
 python app.py
-sleep 30
+sleep 15
 cd output
-curl "http://api2.nitestats.com/v1/itemshop?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
+curl "http://api2.nitestats.com/v1/news/image?background=https://github.com/kuletxcore/kxc-fortnite-bot/raw/master/media/news.jpg" --output news.png
 cd ..
 python news.py
 exit
