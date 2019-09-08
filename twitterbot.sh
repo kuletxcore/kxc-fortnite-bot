e372ab02d8ac186a75712d2607cb8a25ba7bebf7
kuletxcore <kuletxcore@gmail.com>
2019-09-08 11:28:20 +0800

    added delay to the news post
    Because it does not update as is.

--- twitterbot.sh
+++ twitterbot.sh
@@ -4,8 +4,11 @@ rm news.png
 rm shop.png
 sleep 15
 curl "http://api2.nitestats.com/v1/news/image?background=https://github.com/kuletxcore/kxc-fortnite-bot/raw/master/media/news.jpg" --output news.png
-curl "http://api2.nitestats.com/v1/itemshop?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
 cd ..
 python app.py
+sleep 30
+cd output
+curl "http://api2.nitestats.com/v1/itemshop?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
+cd ..
 python news.py
 exit
