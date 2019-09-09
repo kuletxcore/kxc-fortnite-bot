f6bef4de31ed145af567fb40c6da6b3e35c83522
kuletxcore <kuletxcore@gmail.com>
2019-09-09 09:36:48 +0800

    Update twitterbot.sh

--- twitterbot.sh
+++ twitterbot.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
+sleep 15s #If this bot runs anyway and doesn't update the newsbot, I am going to lose my mind rn
 cd output
 rm news.png
 rm shop.png
-sleep 15
+sleep 15s
 curl "http://api2.nitestats.com/v1/itemshop?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
 cd ..
 python app.py
-sleep 15
+sleep 20s
 cd output
 curl "http://api2.nitestats.com/v1/news/image?background=https://github.com/kuletxcore/kxc-fortnite-bot/raw/master/media/news.jpg" --output news.png
 cd ..
