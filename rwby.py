6a56db45fba8c5dea0fb64b73e75e2e1535e3a2d
unknown <kuletxcore@gmail.com>
2019-10-08 15:27:25 +0800

    Create rwby.py
    I WILL POST THIS EVERYDAY UNTIL DONALD MUSTARD WATCHES IT

---
+++ rwby.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import os
+import time
+from twython import Twython, TwythonError
+
+CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
+OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
+TWEET_LENGTH = 280
+TWEET_URL_LENGTH = 21
+
+# RUN_EVERY_N_SECONDS = 86400 # e.g. 60*5 = tweets every five minutes
+
+def twitter_handle():
+    return Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+
+tweetStr = "This bot will post this RWBY playlist every day until @DonaldMustard watches it, or if a @FortniteGame x @OfficialRWBY collabo happens.\nSorry for the inconvenience.\nhttps://www.youtube.com/playlist?list=PL2Oan_kP2lqq2Kc1_tzbYniNWwklzStG1\n\n[Automatically Posted]"
+
+api = twitter_handle()
+api.update_status(status=tweetStr)	
+
+print("Tweeted: " + tweetStr)
