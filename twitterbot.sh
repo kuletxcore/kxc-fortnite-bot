fd808ca41690a9dfa060df27ac48154f9400cc39
kuletxcore <kuletxcore@gmail.com>
2019-10-13 08:31:45 +0800

    Updated shop grabber

--- twitterbot.sh
+++ twitterbot.sh
@@ -4,7 +4,7 @@ cd output
 rm news.png
 rm shop.png
 sleep 15s
-curl "http://api2.nitestats.com/v1/itemshop?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
+curl "http://api2.nitestats.com/v1/shop/image?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
 cd ..
 python app.py
 sleep 20s
