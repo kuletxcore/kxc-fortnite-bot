791e35b6716bb80360125ae146d3f42bd1306d3b
kuletxcore <kuletxcore@gmail.com>
2019-11-05 22:47:40 +0800

    API moved to the main servers!

--- twitterbot.sh
+++ twitterbot.sh
@@ -4,12 +4,12 @@ cd output
 rm news.png
 rm shop.png
 sleep 15s
-curl "http://api2.nitestats.com/v1/shop/image?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
+curl "http://api.nitestats.com/v1/shop/image?background=https://raw.githubusercontent.com/kuletxcore/kxc-fortnite-bot/master/media/nitestats.jpg" --output shop.png
 cd ..
 python app.py
 sleep 20s
 cd output
-curl "http://api2.nitestats.com/v1/news/image?background=https://github.com/kuletxcore/kxc-fortnite-bot/raw/master/media/news.jpg" --output news.png
+curl "http://api.nitestats.com/v1/news/image?background=https://github.com/kuletxcore/kxc-fortnite-bot/raw/master/media/news.jpg" --output news.png
 cd ..
 python news.py
 exit
