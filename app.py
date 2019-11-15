37c9ea452d89714bce803f1647cec4f27c1ca9e0
kuletxcore <kuletxcore@gmail.com>
2019-11-15 16:00:29 +0800

    Update app.py
    added comments

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+#Please do not change anything to this section!
 import sys
 import os
 import time
@@ -12,16 +14,18 @@ OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
 OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
 TWEET_LENGTH = 280
 TWEET_URL_LENGTH = 21
+#Please so not change anything to this section!
 
-# RUN_EVERY_N_SECONDS = 86400 # e.g. 60*5 = tweets every five minutes
 today = datetime.now()
-tz = pytz.timezone('Asia/Manila')
-pht = datetime.now(tz)
+tz = pytz.timezone('Asia/Manila') # change the timezone to your desired/home timezone.
+pht = datetime.now(tz) #change the variable too if you want
 
 def twitter_handle():
     return Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 photo = open('./output/shop.png', 'rb')
+
+#Change the text into anything you like! (and please change the variables accordingly if you changed it above.)
 tweetStr = 'Fortnite item shop for '+pht.strftime("%m/%d/%y")+'!\n'+pht.strftime("%I:%M %p")+', PHT/GMT+8\n\nIf you want to support me, make sure to use code \"KuletXCore\" on the Fortnite Item Shop!\nReally appreciate it!'
 
 api = twitter_handle()
