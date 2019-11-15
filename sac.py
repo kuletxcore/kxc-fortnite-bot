4da6e3896904059cc53dff35b66269866e3ac2a3
kuletxcore <kuletxcore@gmail.com>
2019-11-15 15:57:48 +0800

    Update sac.py
    added comments

--- sac.py
+++ sac.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+#Please do not change anything to this section!
 import sys
 import os
 import time
@@ -11,12 +13,13 @@ OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
 TWEET_LENGTH = 280
 TWEET_URL_LENGTH = 21
 
-# RUN_EVERY_N_SECONDS = 86400 # e.g. 60*5 = tweets every five minutes
 
 def twitter_handle():
     return Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 photo = open('./output/SAC_Reminder.png', 'rb')
+
+#Change the text into anything you like!
 tweetStr = "Weekly Reminder that FN codes expire every Fortnight! (aka 14 days!)\nUse code \"KuletXCore\" in the shop if you want to support me!\n\n[Automatically Posted]"
 
 api = twitter_handle()
